5457000c67136e4afbebdf4169dd421c28d5e9a6
Juan Fernando Gonzalez <t-juagon@microsoft.com>
2020-07-07 00:00:44 -0700

    nando

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,21 +2,15 @@ import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 
-<<<<<<< HEAD
+HEAD
 const HomeScreen = props => {
   return (
     <View>
-   <Text style={styles.text}>Hello David, Mel and Alida, this is the first step of our app!</Text>
+   <Text style={styles.text}>HomeScreen Master Schedule App</Text>
    <Button 
    onPress = {() => props.navigation.navigate('UserLogin')}
    title= "Enter to welcome page"
    />
-=======
-const HomeScreen =({navigation}) => {
-  return ( <View>
-   <Text style={styles.text}> Mel changes Alida, this is the first step of our app!</Text>
-   <Text style={styles.text}>Holaaaa</Text>
->>>>>>> 21855d06c5e9769b7e352dc9756bec76b0295028
    </View>
   );
 };
