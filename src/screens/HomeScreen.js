f4e092980b5932484ab25a985706853d3595698d
Juan Fernando Gonzalez <t-juagon@microsoft.com>
2020-07-06 22:53:12 -0700

    <View>

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,9 +3,10 @@ import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 
 const HomeScreen =({navigation}) => {
-  return (
+  return ( <View>
    <Text style={styles.text}> Mel changes Alida, this is the first step of our app!</Text>
    <Text style={styles.text}>hello, another change</Text>
+   </View>
   );
 };
 
